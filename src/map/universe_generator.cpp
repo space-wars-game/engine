64a08af39ff492213628c0cd55694cc78628b483
Héctor Ramón <hector0193@gmail.com>
2015-12-08 01:08:10 +0100

    Fix random call

--- src/map/universe_generator.cpp
+++ src/map/universe_generator.cpp
@@ -13,14 +13,15 @@ UniverseGenerator::UniverseGenerator(int seed) : seed_(seed) {
 }
 
 void UniverseGenerator::Generate(Universe& universe) {
-  seed({seed_, (int)Elements::SOLAR_SYSTEM, (int)Elements::SUN, 0});
+  seed({seed_, Elements::SOLAR_SYSTEM, Elements::SUN, 0});
 
   universe.planetary_system = new PlanetarySystem;
-  universe.planetary_system->sun = new Sun(Sun::G, Sun::MIN_RADIUS + random() % (Sun::MAX_RADIUS - Sun::MIN_RADIUS));
+  universe.planetary_system->sun = new Sun(Sun::G, Sun::MIN_RADIUS + random_() % (Sun::MAX_RADIUS - Sun::MIN_RADIUS));
 }
 
 void UniverseGenerator::seed(const std::vector<int>& seeds) {
   std::seed_seq sseq(seeds.begin(), seeds.end());
   random_.seed(sseq);
 }
+
 }
