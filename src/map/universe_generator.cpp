447c4bc04deb2392ca0b5627285c278e2fcb9d82
Héctor Ramón <hector0193@gmail.com>
2015-12-08 01:14:43 +0100

    Abstract seed method even more

--- src/map/universe_generator.cpp
+++ src/map/universe_generator.cpp
@@ -13,13 +13,18 @@ UniverseGenerator::UniverseGenerator(int seed) : seed_(seed) {
 }
 
 void UniverseGenerator::Generate(Universe& universe) {
-  seed({seed_, Elements::SOLAR_SYSTEM, Elements::SUN, 0});
+  seed({Elements::SOLAR_SYSTEM, Elements::SUN, 0});
 
   universe.planetary_system = new PlanetarySystem;
   universe.planetary_system->sun = new Sun(Sun::G, Sun::MIN_RADIUS + random_() % (Sun::MAX_RADIUS - Sun::MIN_RADIUS));
 }
 
-void UniverseGenerator::seed(const std::vector<int>& seeds) {
+void UniverseGenerator::seed(const std::vector<int>& elements) {
+  std::vector<int> seeds;
+
+  seeds.push_back(seed_);
+  seeds.insert(seeds.end(), elements.begin(), elements.end());
+
   std::seed_seq sseq(seeds.begin(), seeds.end());
   random_.seed(sseq);
 }
